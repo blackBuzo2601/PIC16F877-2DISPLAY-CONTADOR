cb12e8367080977cf8ace5b987000eee6cc53804
blackBuzo2601 <elianpro58@gmail.com>
2024-03-09 13:28:52 -0800

    Escenario Inicial listo

--- dos display para practicar.asm
+++ dos display para practicar.asm
@@ -5,8 +5,10 @@ LIST p=16F877
 INCLUDE <P16F877.INC>
 
 ORG 0x00 ;Inicio de programa
-DATO EQU 0x20 ;usamos la dirección de memoria 23   
+DATO EQU 0x20 ;usamos la dirección de memoria 23  
+DECENAS EQU 0x21 ;Usamos la direccion de memoria 24
 
+CLRF PORTC ;limpiar registro PORTC
 CLRF PORTB ;limpiar registro PORTB
 CLRF PORTD ;limpiar registro PORTD
 
@@ -16,21 +18,27 @@ BSF STATUS,RP0  ;Acceder al banco 1 puesto que en el banco 1 se encuentran los r
 
 MOVLW b'11111111' ;establecer todos los pines de puerto B como ENTRADA (recibirán valores desde el exterior)
 MOVWF TRISB
-
 MOVLW b'00000000' ;establecer todos los pines de puerto D como SALIDA (Serán manipulados desde dentro)
 MOVWF TRISD
+MOVLW b'00000000' ;establecer todos los pines del puerto C como SALIDA (Serán manipulados desde dentro)
+MOVWF TRISC
 
 BCF STATUS,RP0 ;limpiar RP0 para regresar al BANCO 0.
 
 
+
+
 MOVLW b'00000000' ;inicializar variable DATO en ceros
 MOVWF DATO
+MOVLW b'00000000' ;inicializar variable DECENAS en ceros
+MOVWF DECENAS
 
 
-
+;Instruccion para que al correr el programa comience mostrando el CERO INICIAL.
 MOVF DATO,W ;mover 00000000 a W
 CALL TABLA;llamar a subrutina
 MOVWF PORTD
+
 
 INICIO
 BTFSS PORTB,0 ;checar que el BIT 0 este prendido (boton presionado)
@@ -43,18 +51,9 @@ MOVF DATO,W ;copiar registro DATO a registro W
 CALL TABLA
 MOVWF PORTD
 
-GOTO INICIO
+	GOTO INICIO
 
 
-APAGADO
-MOVLW b'00000000'
-MOVWF PORTD
-BTFSS PORTB,0
-GOTO $-1
-BTFSC PORTB,0
-GOTO $-1
-GOTO INICIO
-	
 	
 ;PCL (Program Counter Low)
 TABLA
@@ -69,9 +68,8 @@ RETLW b'01111101' 	  ;SEIS
 RETLW b'00000111'	  ;SIETE
 RETLW b'01111111' 	  ;OCHO
 RETLW b'01100111'	  ;NUEVE
-GOTO APAGADO
 
-TABLA_DOS
+TABLA_DECENAS
 RETLW b'00111111'     ;CERO
 RETLW b'00000110' 	  ;UNO
 RETLW b'01011011' 	  ;DOS
@@ -84,7 +82,21 @@ RETLW b'01111111' 	  ;OCHO
 RETLW b'01100111'	  ;NUEVE
 
 
-END
+ALMACENADOR_DECENAS
 
-;00001001 NUEVE
-;00001010 DIEZ
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+END ;DELIMITADOR FIN DEL PROGRAMA
+
